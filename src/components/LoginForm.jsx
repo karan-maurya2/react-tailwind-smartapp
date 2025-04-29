62f9397f5bc3fb6d5f20357c1cad814dd747b616
Karan Maurya <karanmaurya.connect@gmail.com>
2025-04-29 23:02:49 +0530

    code updated

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -6,8 +6,8 @@ function LoginForm(){
     return(
         <>
         
-        <form action="https://example.com" method="get" className="flex flex-col bg-yellow-100">
-            <div className="flex flex-col items-center justify-center min-h-60 bg-gray-100 border-2 border-red-500 ">
+        <form action="https://example.com" method="get" className="flex flex-col bg-blue-600">
+            <div className="flex flex-col items-center justify-center min-h-64 bg-green-700 border-2 border-red-500 ">
                 <h1 className="text-2xl font-bold mb-4">Login</h1>
                 <input type="text" placeholder="Username" className="mb-4 p-2 border rounded-lg" />
                 <input type="password" placeholder="Password" className="mb-4 p-2 border rounded-lg" />
