29873fc7e6f9343454da83c2db2449443c1383a4
Karan Maurya <karanmaurya.connect@gmail.com>
2025-05-07 23:54:57 +0530

    code updated

--- src/components/FileFromTerminal.jsx
+++ src/components/FileFromTerminal.jsx
@@ -8,8 +8,21 @@ function calculation(params) {
        <div>
 
         <h1>Calculation</h1>
-        
+      
        </div>
+       <div>
+  <div class="bg-sky-50"></div>
+  <div class="bg-sky-100"></div>
+  <div class="bg-sky-200"></div>
+  <div class="bg-sky-300"></div>
+  <div class="bg-sky-400"></div>
+  <div class="bg-sky-500"></div>
+  <div class="bg-sky-600"></div>
+  <div class="bg-sky-700"></div>
+  <div class="bg-sky-800"></div>
+  <div class="bg-sky-900"></div>
+  <div class="bg-sky-950"></div>
+</div>
         
         </>
     )
