c2ba0fcceab3456669fb90a5227171db0d631ce8
Karan Maurya <karanmaurya.connect@gmail.com>
2025-05-09 23:46:39 +0530

    code updated

--- src/components/FileFromTerminal.jsx
+++ src/components/FileFromTerminal.jsx
@@ -11,17 +11,17 @@ function calculation(params) {
       
        </div>
        <div>
-  <div class="bg-sky-50"></div>
-  <div class="bg-sky-100"></div>
-  <div class="bg-sky-200"></div>
-  <div class="bg-sky-300"></div>
-  <div class="bg-sky-400"></div>
-  <div class="bg-sky-500"></div>
-  <div class="bg-sky-600"></div>
-  <div class="bg-sky-700"></div>
-  <div class="bg-sky-800"></div>
-  <div class="bg-sky-900"></div>
-  <div class="bg-sky-950"></div>
+  <div class="bg-sky-50">Box1</div>
+  <div class="bg-sky-100">Box2</div>
+  <div class="bg-sky-200">Box3</div>
+  <div class="bg-sky-300">Box4</div>
+  <div class="bg-sky-400">Box5</div>
+  <div class="bg-sky-500">Box6</div>
+  <div class="bg-sky-600">Box7</div>
+  <div class="bg-sky-700">Box8</div>
+  <div class="bg-sky-800">Box9</div>
+  <div class="bg-sky-900">Box10</div>
+  <div class="bg-sky-950">Box11</div>
 </div>
         
         </>
