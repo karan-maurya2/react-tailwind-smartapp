5b5ad1e9ea59a38713a82cdad47fe833ff5467cc
Karan Maurya <karanmaurya.connect@gmail.com>
2025-05-10 23:49:25 +0530

    code updated

--- src/components/FileFromTerminal.jsx
+++ src/components/FileFromTerminal.jsx
@@ -1,4 +1,4 @@
-function calculation(params) {
+function calculation() {
 
 
     return(
@@ -6,22 +6,20 @@ function calculation(params) {
         <>
         
        <div>
-
-        <h1>Calculation</h1>
       
        </div>
-       <div>
-  <div class="bg-sky-50">Box1</div>
-  <div class="bg-sky-100">Box2</div>
-  <div class="bg-sky-200">Box3</div>
-  <div class="bg-sky-300">Box4</div>
-  <div class="bg-sky-400">Box5</div>
-  <div class="bg-sky-500">Box6</div>
-  <div class="bg-sky-600">Box7</div>
-  <div class="bg-sky-700">Box8</div>
-  <div class="bg-sky-800">Box9</div>
-  <div class="bg-sky-900">Box10</div>
-  <div class="bg-sky-950">Box11</div>
+       <div className="flex flex-row">
+  <div className="bg-orange-500">Box1</div>
+  <div className="bg-sky-100">Box2</div>
+  <div className="bg-sky-200">Box3</div>
+  <div className="bg-sky-300">Box4</div>
+  <div className="bg-sky-400">Box5</div>
+  <div className="bg-sky-500">Box6</div>
+  <div className="bg-sky-600">Box7</div>
+  <div className="bg-sky-700">Box8</div>
+  <div className="bg-sky-800">Box9</div>
+  <div className="bg-sky-900">Box10</div>
+  <div className="bg-sky-950">Box11</div>
 </div>
         
         </>
