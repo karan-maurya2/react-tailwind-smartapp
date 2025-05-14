b3255723ad7da3bf4fac6c5df34f10439d40fef4
Karan Maurya <karanmaurya.connect@gmail.com>
2025-05-14 23:50:54 +0530

    code updated

--- src/components/FileFromTerminal.jsx
+++ src/components/FileFromTerminal.jsx
@@ -20,6 +20,18 @@ function calculation() {
   <div className="bg-sky-800">Box9</div>
   <div className="bg-sky-900">Box10</div>
   <div className="bg-sky-950">Box11</div>
+  <ul role="list">
+  
+    {/* <!-- Remove top/bottom padding when first/last child --> */}
+    <li className="flex py-4 first:pt-0 last:pb-0">
+      <img className="h-10 w-10 rounded-full" src={person.imageUrl} alt="" />
+      <div className="ml-3 overflow-hidden">
+        <p className="text-sm font-medium text-gray-900 dark:text-white">{person.name}</p>
+        <p className="truncate text-sm text-gray-500 dark:text-gray-400">{person.email}</p>
+      </div>
+    </li>
+ 
+</ul>
 
   <input type="button" value="Click Me" />
   
