0635031ae99e1ab59a00c6f60814fb1da0084b5f
Karan Maurya <karanmaurya.connect@gmail.com>
2025-07-07 22:59:43 +0530

    code update

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -17,6 +17,7 @@ function LoginForm(){
             </div>
         </form>
         </div>
+        
 </>
     );
 
---
+++ src/components/Prepanel.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const Prepanel = () => {
+
+    return(
+
+        < >
+<div>
+
+    <h1>Prepanel</h1>
+</div>
+
+        </>
+    )
+
+}
+
+
+export default Prepanel;
