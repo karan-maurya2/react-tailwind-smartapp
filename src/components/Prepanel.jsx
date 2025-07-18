ce4f48a2331195ed1055fa01a94bfe1cb1af5f8c
Karan Maurya <karanmaurya.connect@gmail.com>
2025-07-18 22:44:58 +0530

    code

---
+++ src/components/Prepanel.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+const Prepanel = () => {
+
+    return(
+
+        < >
+<div>
+
+    <h1>Prepanel</h1>
+
+    <><h2>Subheading</h2></>
+</div>
+
+
+
+        </>
+    )
+
+}
+
+
+export default Prepanel;
